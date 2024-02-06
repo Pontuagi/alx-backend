da00c7b65818425933bf7a4a122584d1a2078d6b
Pontuagi <benepontuagi@gmail.com>
2024-02-06 12:04:31 +0300

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+import redis from 'redis';
+
+const subscriberClient = redis.createClient();
+
+subscriberClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+subscriberClient.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+subscriberClient.subscribe('holberton school channel');
+
+subscriberClient.on('message', (channel, message) => {
+  console.log(`${message}`);
+  if (message === 'KILL_SERVER') {
+    subscriberClient.unsubscribe();
+    subscriberClient.quit();
+  }
+});
